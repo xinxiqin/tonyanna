50f3525b624f8e3f12cd82c3836fc8e01772761e
Xinxi Qin <xinxi.qin@gmail.com>
2025-07-05 22:43:33 -0400

    change pic location

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
       <header className="App-header">
         <h1>love... peace...</h1>
         <p>Enson, 06/06/2025</p>
-        <img src="./pictures/anna_dad_enson.HEIC" width="403" height="302" alt="anna_dad" />
+        <img src="./anna_dad_enson.HEIC" width="403" height="302" alt="anna_dad" />
         <p>UNC, 04/08/2025</p>
-        <img src="./pictures/tony_and_parents_unc.JPEG" width="428" height="571" alt="anna_dad" />
+        <img src="./tony_and_parents_unc.JPEG" width="428" height="571" alt="anna_dad" />
       </header>
       
     </div>
